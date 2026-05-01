5c1970ff93e110500e933d9dd2875b214382b917
conctanleo <2332729@tongji.edu.cn>
2026-05-01 16:14:28 +0800

    feat: wire up routing and survey data loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,47 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { useEffect } from 'react';
+import { Spin } from 'antd';
+import useSurveyStore from './stores/surveyStore';
+import { fetchSurvey } from './api/survey';
+import InfoCollection from './pages/InfoCollection';
+import Welcome from './pages/Welcome';
+import Survey from './pages/Survey';
+import Complete from './pages/Complete';
+
+function SurveyLoader({ children }) {
+  const surveyId = useSurveyStore((s) => s.surveyId);
+  const setSurveyData = useSurveyStore((s) => s.setSurveyData);
+  const loading = useSurveyStore((s) => !s.surveyConfig);
+
+  useEffect(() => {
+    if (!surveyId) {
+      const path = window.location.pathname.split('/')[1];
+      fetchSurvey(path).then((data) => setSurveyData(data));
+    }
+  }, [surveyId, setSurveyData]);
+
+  if (loading) {
+    return (
+      <div style={{ display: 'flex', justifyContent: 'center', alignItems: 'center', height: '100vh', background: '#4f46e5' }}>
+        <Spin size="large" />
+      </div>
+    );
+  }
+
+  return children;
+}
 
 export default function App() {
   return (
     <BrowserRouter>
-      <Routes>
-        <Route path="/:surveyId" element={<div>Survey App Ready</div>} />
-      </Routes>
+      <SurveyLoader>
+        <Routes>
+          <Route path="/:surveyId" element={<InfoCollection />} />
+          <Route path="/:surveyId/welcome" element={<Welcome />} />
+          <Route path="/:surveyId/survey" element={<Survey />} />
+          <Route path="/:surveyId/complete" element={<Complete />} />
+        </Routes>
+      </SurveyLoader>
     </BrowserRouter>
   );
 }
